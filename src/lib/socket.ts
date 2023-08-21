d49323b4757ad5e3a50f2880cb110da77236c200
Ankit Kumar <112799319+Ankit8295@users.noreply.github.com>
2023-08-21 16:25:53 +0530

    fix

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,8 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io(
-  "http://ec2-13-126-102-15.ap-south-1.compute.amazonaws.com:3001",
-  {
-    autoConnect: false,
-  }
-);
+export const socket = io("https://socket.ankitdev.in:3001", {
+  autoConnect: false,
+});
