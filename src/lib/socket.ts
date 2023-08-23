8577473bb38b930f73b3d1b9f110d0e3cb4508d1
Ankit Kumar <112799319+Ankit8295@users.noreply.github.com>
2023-08-23 11:36:05 +0530

    Update socket.ts

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://xenons.ankitdev.in:3001", {
+export const socket = io("http://xenons.ankitdev.in:3001", {
   autoConnect: false,
   secure:true,
 });
