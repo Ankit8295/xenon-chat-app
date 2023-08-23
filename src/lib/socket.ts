a1c57a924e1d2403035d39540c87c6f265bee354
manimansih <99656925+anime1nation@users.noreply.github.com>
2023-08-23 21:52:05 +0530

    fix; socket api

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://xenons.ankitdev.in:3001", {
+export const socket = io("https://xenons.ankitdev.in:3001", {
   autoConnect: false,
 });
