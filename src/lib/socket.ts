2b45f073cb648078ad6434d9dd4f0fa6057eebca
manimansih <99656925+anime1nation@users.noreply.github.com>
2023-08-23 23:01:10 +0530

    fix: socket

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("3.6.108.237:3001", {
+export const socket = io("http://3.6.108.237:3001", {
   autoConnect: false,
   secure:true,
 });
