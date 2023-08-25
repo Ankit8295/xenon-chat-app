9e65e68996bed4d9c316a1ecc64eaa463879831c
Ankit Kumar <112799319+Ankit8295@users.noreply.github.com>
2023-08-25 15:49:09 +0530

    added subdomain to socket

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://xenonchat.ankitdev.in:8443/", {
+export const socket = io("https://xenonchatsocket.ankitdev.in:8443/", {
   autoConnect: false,
   secure: true,
 });
