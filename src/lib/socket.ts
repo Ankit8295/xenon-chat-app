c5b0af60be0a80cea2ad63c0b2518eb0bf6e68e4
Ankit Kumar <112799319+Ankit8295@users.noreply.github.com>
2023-08-26 11:21:11 +0530

    fix

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.NEXT_PUBLIC_SOCKET_SERVER, {
+export const socket = io("https://xenonchat.ankitdev.in:8443/", {
   autoConnect: false,
   secure: true,
 });
