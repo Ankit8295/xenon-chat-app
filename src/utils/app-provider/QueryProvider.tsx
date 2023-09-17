f78ce340207ea247bf2b902ca21a582ef618ac39
Ankit Kumar <112799319+Ankit8295@users.noreply.github.com>
2023-09-17 15:19:54 +0530

    fix

--- src/utils/app-provider/QueryProvider.tsx
+++ src/utils/app-provider/QueryProvider.tsx
@@ -8,7 +8,7 @@ export default function QueryProvider({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   );
 }
