f166b90f75823146f29c6feaefcb35b561887799
Ankit Kumar <112799319+Ankit8295@users.noreply.github.com>
2023-09-18 10:37:53 +0530

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,11 +13,20 @@ export const revalidate = 30;
 export const metadata = {
   title: "Xenon Chat",
   description: "chat with friends with Xenon Chat",
+  generator: "Ankit Kumar",
+  referrer: "origin-when-cross-origin",
+  keywords: ["Chat", "Message", "ChatApp", "Xenon"],
+  authors: [{ name: "Ankit Kumar" }],
+  publisher: "Ankit Kumar",
+  colorScheme: "dark",
+  category: "Messaging",
   viewport: {
     width: "device-width",
     initialScale: 1,
     maximumScale: 1,
   },
+  manifest: process.env.MANIFEST_LINK,
+  bookmarks: [process.env.BOOKMARK_LINK],
 };
 
 export default function RootLayout({
