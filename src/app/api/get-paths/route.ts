53c95b7907af4b57cc40049d2b3acb14ea42159d
Ankit Kumar <112799319+Ankit8295@users.noreply.github.com>
2023-09-24 09:58:01 +0530

    fix

--- src/app/api/get-paths/route.ts
+++ src/app/api/get-paths/route.ts
@@ -5,7 +5,6 @@ export async function GET(request: Request) {
   const dataBase = await db();
   const users = await dataBase.collection("users").find().toArray();
   const paths = users.map((user) => user.userName);
-  console.log(paths);
   return NextResponse.json({
     status: 200,
     data: paths,
--- src/app/home/[friendId]/page.tsx
+++ src/app/home/[friendId]/page.tsx
@@ -11,13 +11,17 @@ type Params = {
 export const dynamicParams = false;
 
 export async function generateStaticParams() {
-  const paths = await fetch(`${process.env.NEXTAUTH_URL}/api/get-paths`, {
-    cache: "no-store",
-  }).then((res) => res.json());
-
-  return paths.data.map((path: UserDb) => ({
-    friendId: path,
-  }));
+  try {
+    const paths = await fetch(`${process.env.NEXTAUTH_URL}/api/get-paths`, {
+      cache: "no-store",
+    }).then((res) => res.json());
+    return paths.data.map((path: UserDb) => ({
+      friendId: path,
+    }));
+  } catch (err) {
+    console.log(err);
+    throw new Error();
+  }
 }
 
 export default function Page({ params }: Params) {
