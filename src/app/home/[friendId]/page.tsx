fdd115a94e03e68d59ba3adf65971f29cb9a7860
Ankit Kumar <112799319+Ankit8295@users.noreply.github.com>
2023-09-24 09:55:27 +0530

    fix

--- src/app/home/[friendId]/page.tsx
+++ src/app/home/[friendId]/page.tsx
@@ -14,14 +14,13 @@ export async function generateStaticParams() {
   const paths = await fetch(`${process.env.NEXTAUTH_URL}/api/get-paths`, {
     cache: "no-store",
   }).then((res) => res.json());
-  if (paths) {
-    return paths.data.map((path: UserDb) => ({
-      friendId: path,
-    }));
-  }
-  return NotFound();
+
+  return paths.data.map((path: UserDb) => ({
+    friendId: path,
+  }));
 }
 
 export default function Page({ params }: Params) {
-  return <FriendId friendId={params.friendId} />;
+  if (params.friendId) return <FriendId friendId={params.friendId} />;
+  return NotFound();
 }
