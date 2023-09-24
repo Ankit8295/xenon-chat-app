68e3bf9f8f5e7e4e38278cf621d9c420bf544a4c
Ankit Kumar <112799319+Ankit8295@users.noreply.github.com>
2023-09-24 19:10:34 +0530

    fix

--- src/app/home/[friendId]/page.tsx
+++ src/app/home/[friendId]/page.tsx
@@ -1,5 +1,4 @@
-import FriendId from "@/src/components/firendId/FriendId";
-import { UserDb } from "@/src/utils/types/types";
+import FriendId from "@/src/components/friendId/FriendId";
 import NotFound from "../../not-found";
 
 type Params = {
@@ -7,22 +6,6 @@ type Params = {
     friendId: string;
   };
 };
-
-export const dynamicParams = false;
-
-export async function generateStaticParams() {
-  try {
-    const paths = await fetch(`${process.env.NEXTAUTH_URL}/api/get-paths`, {
-      cache: "no-store",
-    }).then((res) => res.json());
-    return paths.data.map((path: UserDb) => ({
-      friendId: path,
-    }));
-  } catch (err) {
-    console.log(err);
-    throw new Error();
-  }
-}
 
 export default function Page({ params }: Params) {
   if (params.friendId) return <FriendId friendId={params.friendId} />;
